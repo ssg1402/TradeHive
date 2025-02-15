714761979f88f28506f48d893067a52cc5f9d3ed
siddhant <ishusingh@gmail.com>
2025-02-15 12:45:04 +0530

    commit for first time

---
+++ app/api/stores/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse, NextRequest } from "next/server";
+import { currentUser, getAuth } from "@clerk/nextjs/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+    req:NextRequest,
+) {
+    try {
+        const auth = getAuth(req);
+    console.log("Auth Data:", auth);
+        
+        const user = await currentUser();
+        console.log("🟢 Current Clerk User:", user);
+
+        const body =  await req.json();
+
+        const { name } = body;
+const { userId } = auth;
+        if(!userId){
+            return new NextResponse("Unauthorized", {status : 401});
+        }
+        if(!name){
+            return new NextResponse("name is required", {status : 400});
+        }
+
+        const store = await prismadb.store.create({
+            data: {
+                name,
+                userId
+            }
+        });
+        
+        return NextResponse.json(store);
+
+    } catch (error) {
+        console.log('[STORES_POST]',error);
+        return new NextResponse("Internal Error", {status : 500});
+    }
+}
